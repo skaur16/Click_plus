7690ee962b7b879610b55db4323b7a7d96f0c777
skaur16 <me.sharandeepkaur16@gmail.com>
2024-01-13 15:26:50 +0530

    States created and updated in design..

--- app/src/main/java/com/example/clickplus/MainActivity.kt
+++ app/src/main/java/com/example/clickplus/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.clickplus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +21,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val stateView : PracticeViewModel by viewModels()
+
             ClickPlusTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center
                     )
                     {
-                        Text(text = "0")
+                        Text(text = stateView.count.value.toString())
                         Button(onClick = { /*TODO*/ }) {
                             Text(text = "Click me!")
                         }
---
+++ app/src/main/java/com/example/clickplus/PracticeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.clickplus
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class PracticeViewModel : ViewModel(){
+    val count = mutableStateOf(0)
+
+    fun count(){
+
+    }
+
+}
