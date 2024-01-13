50020b225871831b2edaf190608a8f698fafb28a
skaur16 <me.sharandeepkaur16@gmail.com>
2024-01-13 15:14:21 +0530

    Design created!

--- app/src/main/java/com/example/clickplus/MainActivity.kt
+++ app/src/main/java/com/example/clickplus/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.clickplus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.clickplus.ui.theme.ClickPlusTheme
@@ -22,25 +26,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    )
+                    {
+                        Text(text = "0")
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Click me!")
+                        }
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClickPlusTheme {
-        Greeting("Android")
-    }
-}
